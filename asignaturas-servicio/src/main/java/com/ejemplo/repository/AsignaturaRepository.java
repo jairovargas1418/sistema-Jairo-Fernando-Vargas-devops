63e9d82262436c0b3c5b33d8289824bc8c5e5ba5
Jairo Fernando Vargas Pimiento <94024758+jairovargas1418@users.noreply.github.com>
2025-04-23 10:47:07 -0500

    Delete asignaturas-servicio directory

--- asignaturas-servicio/src/main/java/com/ejemplo/repository/AsignaturaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ejemplo.repository;
-
-import com.ejemplo.model.Asignatura;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AsignaturaRepository extends MongoRepository<Asignatura, String> {
-    Asignatura findByCodigo(String codigo); // Buscar asignatura por código único
-}
