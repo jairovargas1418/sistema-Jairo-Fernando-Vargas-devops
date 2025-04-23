e6c72b89dc69baa87a02f36ca36f9d951a9b7792
Jairo Fernando Vargas Pimiento <94024758+jairovargas1418@users.noreply.github.com>
2025-04-23 10:47:46 -0500

    Delete usuarios-servicios directory

--- usuarios-servicios/src/main/java/com/example/usuariosservicios/security/JwtFilter.java
+++
@@ -1,5 +0,0 @@
-package com.example.usuariosservicios.security;
-
-public class JwtFilter {
-
-}
--- usuarios-servicios/src/main/java/org/springframework/cloud/openfeign/EnableFeignClients.java
+++
@@ -1,5 +0,0 @@
-package org.springframework.cloud.openfeign;
-
-public record EnableFeignClients() {
-
-}
